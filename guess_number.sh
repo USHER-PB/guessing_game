d37807b091588cf32a26c35a524f6ea5c9ad6eb0
Tchankoumi Usher <ushertchankoumi9@gmail.com>
2024-09-30 15:41:29 +0100

    some new features have beeen aded

--- guess_number.sh
+++ guess_number.sh
@@ -1,21 +1,23 @@
-guessing_number()
-       rand_number=$((RANDOM % $1))
-         for i in {1..3}
-       echo "Guess the number" #prompting the user to enter a value 
-       read guess_number #read input from the user
-         if [ $rand_number -eq $guess_number ]; then
-              echo " Congratulaltion, You won!"
-              return
-           elif [ $rand_number -gt $guess number ];  then
-               echo "You missed! The random number is greater than $guess_number"
-           else 
-               echo "You missed! The random number is lesser  than $guess_number"
-              "  
-             fi
-             echo "please try again!"
-          done
-           echo "You lose, the number was $rand_number"
-           
-}
+  guessing_number() {
+    rand_number=$((RANDOM % $1))
+    read -p "Guess a number between 0 and $1: " guest_number
+
+    for i in {1..3}; do
+      if [ $rand_number -eq $guest_number ]; then
+        echo "You won!"
+        break
+      elif [ $rand_number -gt $guest_number ]; then
+        echo "You missed! The random number is greater than $guest_number."
+        read -p "Please try again: " guest_number
+      else
+        echo "You missed! The random number is less than $guest_number."
+        read -p "Please try again: " guest_number
+      fi
+    done
+
+    if [ $i -eq 3 ]; then
+      echo "You lose! The correct number was $rand_number."
+    fi
+  }
 
 
